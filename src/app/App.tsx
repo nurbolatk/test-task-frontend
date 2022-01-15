498a3c2bcd8babcfe5470914ea54b2fd43ed5383
nurbolatk <nurbolattk@gmail.com>
2022-01-15 19:09:16 +0600

    Setup eslint + prettier

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React from 'react'
 import './styles/index.css'
 
 function App() {
-  return     <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
